101a91e7073a53c18e40b7c869242d19b2091228
Yaniv Ohayon <yanivohayon8@gmail.com>
2025-03-27 13:00:47 +0200

    test_read_pdf

--- src/pdf_handler.py
+++ src/pdf_handler.py
@@ -1,14 +1,35 @@
 import re
+import pymupdf
 
 def remove_new_line_(text):
     return text.replace("\n"," .")
 
-def remove_references_(text:str):
+def remove_citation_(text:str):
     return re.sub("\[\d+\]","",text)
+
+def remove_references_(text:str):
+    return re.sub("(References)\n*.*","",text,flags=re.DOTALL|re.IGNORECASE)
 
 def clean_(text:str):
     text = remove_new_line_(text)
+    text = remove_citation_(text)
     text = remove_references_(text)
     text = text.strip()
 
     return text 
+
+def read_pdf_text_(input_path):
+    text = None
+
+    with pymupdf.open(input_path) as doc:
+        text = chr(12).join([page.get_text() for page in doc])
+    
+    return text
+
+def read_pdf(input_path):
+    text = read_pdf_text_(input_path)
+    text = clean_(text)
+
+    return text
+
+
--- tests/test_pdf_handler.py
+++ tests/test_pdf_handler.py
@@ -19,6 +19,48 @@ class TestCleaning(unittest.TestCase):
         self.assertNotIn("[7]",text)
         self.assertNotIn("[34]",text)
 
+    
+    def test_read_pdf_internal_func(self):
+        in_path = "tests/data/Harel_et_al-2024-International_Journal_of_Computer_Vision.pdf"
+        text = pdf_handler.read_pdf_text_(in_path)
+        print(text)
+        self.assertNotEqual(len(text),0)
+    
+    def test_remove_references(self):
+        text = """This research was partially supported by
+                References
+                [1] J. Allen. Short term spectral analysis, synthesis, and modification by discrete Fourier transform. IEEE
+                transactions on acoustics, speech, and signal processing, 25(3):235–238, 1977.
+                [2] J. B. Allen and L. R. Rabiner. A unified approach to short-time Fourier analysis and synthesis. Proceedings
+                of the IEEE, 65(11):1558–1564, 1977.
+                [3] S.-I. Amari. Learning patterns and pattern sequences by self-organizing nets of threshold elements. IEEE
+                Transactions on computers, 100(11):1197–1206, 1972.
+                [4] B. D. Anderson. Reverse-time diffusion equation models. Stochastic Processes and their Applications,
+                12(3):313–326, 1982.
+                [5] M. Arjovsky, A. Shah, and Y. Bengio. Unitary evolution recurrent neural networks. In International
+                conference on machine learning. PMLR, 2016.
+                [6] O. Azencot, N. B. Erichson, V. Lin, and M. Mahoney. Forecasting sequential data using consistent
+                Koopman autoencoders. In International Conference on Machine Learning, pages 475–485. PMLR,
+                2020.
+                
+                Apenndix
+                yada yada
+                """
+
+        text = pdf_handler.remove_references_(text)
+        # print(text)
+
+        self.assertEqual(text,"""This research was partially supported by
+                """)
+
+    def test_read_pdf(self):
+        in_path = "tests/data/Harel_et_al-2024-International_Journal_of_Computer_Vision.pdf"
+        text = pdf_handler.read_pdf(in_path)
+        # print(text)
+        self.assertNotEqual(len(text),0)
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
