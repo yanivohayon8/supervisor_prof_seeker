cafe14e18b01b40b511bc284990807a1d35da9c7
Yaniv Ohayon <yanivohayon8@gmail.com>
2025-04-03 12:05:38 +0300

    SimpleRAGChatbot stab

--- src/chatbots/simple.py
+++ src/chatbots/simple.py
@@ -8,8 +8,12 @@ from langchain.chat_models import init_chat_model
 from langgraph.checkpoint.memory import MemorySaver
 import uuid
 from langchain_core.messages import AIMessage
+from langchain_core.documents import Document
+from langchain_core.prompts import ChatPromptTemplate
 
-class State(TypedDict):
+
+
+class SimpleState(TypedDict):
     messages: Annotated[list, add_messages]
 
 class SimpleChatbot:
@@ -18,7 +22,7 @@ class SimpleChatbot:
         verify_openai_api_key()
         self.llm = init_chat_model("gpt-4o-mini", model_provider="openai")
 
-        self.graph_builder = StateGraph(State)
+        self.graph_builder = StateGraph(SimpleState)
         
         self.graph_builder.add_node("chatbot_",self.chatbot_)
         self.graph_builder.add_edge(START, "chatbot_")
@@ -26,7 +30,7 @@ class SimpleChatbot:
 
         self.graph = self.graph_builder.compile(checkpointer=MemorySaver())
     
-    def chatbot_(self,state:State):
+    def chatbot_(self,state:SimpleState):
         return {"messages": [self.llm.invoke(state["messages"])]}
 
     def stream_graph_updates(self, query,config):
@@ -57,4 +61,69 @@ class SimpleChatbot:
                 user_input = "exit"
                 print("User: " + user_input)
                 self.stream_graph_updates(user_input,config)
+                break
+
+
+class SimpleRAGState(TypedDict):
+    question:str
+    context:list[Document]
+    answer:str
+
+class SimpleRAGChatbot():
+    
+    def __init__(self,vector_store):
+        verify_openai_api_key()
+        self.llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+        self.vector_store = vector_store
+
+        self.graph_builder = StateGraph(SimpleRAGState)
+        self.graph_builder.add_node("retrieve_",self.retrieve_)
+        self.graph_builder.add_node("generate_",self.generate_)
+        self.graph_builder.add_edge(START,"retrieve_")
+        self.graph_builder.add_edge("retrieve_","generate_")
+        self.graph_builder.add_edge("generate_",END)
+
+        self.graph = self.graph_builder.compile(checkpointer=MemorySaver())
+
+        self.prompt = ChatPromptTemplate([
+            ("system", "You are an assistant for question-answering tasks. Use the following context to answer." +
+                        " If you do not know the answer say you do not know. Answer concisely."
+                        "\n\n {docs_content}"),
+            ("human", "Question: {question}")
+        ])
+    
+    def retrieve_(self,state:SimpleRAGState):
+        retrieved_docs = self.vector_store.similarity_search(state["question"])
+        return {"context":retrieved_docs}
+
+    def generate_(self, state:SimpleRAGState):
+        docs_content = "\n\n".join([docs.page_content for docs in state["context"]])
+        prompt_value = self.prompt.invoke({"docs_content":docs_content,"question":state["question"]})
+        response = self.llm.invoke(prompt_value)
+
+        return {"answer":response.content}
+    
+    def stream_graph_update(self, user_input,config):
+        for step in self.graph.stream({"question":user_input},config=config):
+            if "generate_" in step.keys():
+                print(step["generate_"]["answer"])
+
+
+    def run(self,thread_id="aaa"):
+        config = {"configurable":{"thread_id":thread_id}}
+
+        while True:
+            try:
+                user_input = input("User:")
+                print()
+                if user_input in ["q", "exit", "quit"]:
+                    print("Goodbye")
+                    break
+                
+                self.stream_graph_update(user_input,config)
+
+            except Exception as e:
+                user_input = "What is Bob?"
+                self.stream_graph_update(user_input,config)
+                print("Goodbye")
                 break
--- tests/test_chatbots.py
+++ tests/test_chatbots.py
@@ -1,12 +1,34 @@
 import unittest
-from src.chatbots.simple import SimpleChatbot
-
+from src.chatbots.simple import SimpleChatbot,SimpleRAGChatbot
+from langchain_community.vectorstores import InMemoryVectorStore
+from langchain_openai import OpenAIEmbeddings
+from src.api_utils import verify_openai_api_key
+from langchain_core.documents import Document
 
 class TestSimple(unittest.TestCase):
 
     def test_run_single_user(self):
         bot = SimpleChatbot()
         bot.run()
+
+class TestSimpleRAGChatbot(unittest.TestCase):
+    def test_run_single_user(self):
+        verify_openai_api_key()
+        embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+        vector_store = InMemoryVectorStore(embeddings)
+
+        docs = [
+            Document(page_content="Bob is to strike with a quick light blow"),
+            Document(page_content="Snark is an informal word that refers to an attitude or expression of mocking irreverence and sarcasm."),
+            Document(page_content="Someone or something described as effusive is expressing or showing a lot of emotion or enthusiasm."),
+            Document(page_content="Penchant refers to a strong liking for something, or a strong tendency to behave in a certain way. It is usually used with for."),
+            Document(page_content="Untoward is a formal word that describes something that is improper or inappropriate, or that is adverse or unfavorable.")
+        ]
+        vector_store.add_documents(docs)
+
+        bot = SimpleRAGChatbot(vector_store)
+        bot.run()
+
 
 if __name__ == "__main__":
     unittest.main()
