41ec575e424cdbf1b54289209f6a8cc1868867c9
Yaniv Ohayon <yanivohayon8@gmail.com>
2025-04-07 11:23:16 +0300

    removing comments

--- scrape_pdfs.py
+++ scrape_pdfs.py
@@ -64,65 +64,8 @@ def scrape_papers(supervisor_folder:str):
 
 if __name__ == "__main__":
     supervisor_folders = [folder for folder in glob("data/google_scholar/*")]
-    # scrape_papers(supervisor_folders[0])
-    # scrape_papers(supervisor_folders[1])
-    # scrape_papers(supervisor_folders[2]) # many blocks (retry?)
-    # scrape_papers(supervisor_folders[3]) 
-    # scrape_papers(supervisor_folders[4]) 
-    # scrape_papers(supervisor_folders[5]) 
-    # scrape_papers(supervisor_folders[6]) 
-    # scrape_papers(supervisor_folders[7]) 
-    # scrape_papers(supervisor_folders[8]) 
-    # scrape_papers(supervisor_folders[9]) 
-    # scrape_papers(supervisor_folders[10]) 
-    # scrape_papers(supervisor_folders[11]) 
-    # scrape_papers(supervisor_folders[12]) 
-    # scrape_papers(supervisor_folders[13]) 
-    # scrape_papers(supervisor_folders[14]) 
-    # scrape_papers(supervisor_folders[15]) 
-    # scrape_papers(supervisor_folders[16]) 
-    # scrape_papers(supervisor_folders[17]) 
-    # scrape_papers(supervisor_folders[18]) 
-    # scrape_papers(supervisor_folders[19]) 
-    # scrape_papers(supervisor_folders[20]) 
-    # scrape_papers(supervisor_folders[21])
 
-    # proces = Process(target=scrape_papers,args=[supervisor_folders[22]])
-    # proces.start()
-    # proces.join()
-    
-    # for i in range(23,25):
-    #     print(f"Starting process of {i}")
-    #     proces = Process(target=scrape_papers,args=[supervisor_folders[i]])
-    #     proces.start()
-    #     proces.join()
-    
-    # for i in range(25,28):
-    #     print(f"********************************* Starting process of {i} *********************************")
-    #     proces = Process(target=scrape_papers,args=[supervisor_folders[i]])
-    #     proces.start()
-    #     proces.join()
-    
-    # for i in range(28,35):
-    #     print(f"********************************* Starting process of {i} *********************************")
-    #     proces = Process(target=scrape_papers,args=[supervisor_folders[i]])
-    #     proces.start()
-    #     proces.join()
-    
-    # for i in range(35,40):
-    #     print(f"********************************* Starting process of {i} *********************************")
-    #     proces = Process(target=scrape_papers,args=[supervisor_folders[i]])
-    #     proces.start()
-    #     proces.join()
-
-    # for i in range(40,43):
-    #     print(f"********************************* Starting process of {i} *********************************")
-    #     proces = Process(target=scrape_papers,args=[supervisor_folders[i]])
-    #     proces.start()
-    #     proces.join()
-    #     time.sleep(5)
-
-    for i in range(43,len(supervisor_folders)):
+    for i in range(len(supervisor_folders)):
         print(f"********************************* Starting process of {i} *********************************")
         proces = Process(target=scrape_papers,args=[supervisor_folders[i]])
         proces.start()
