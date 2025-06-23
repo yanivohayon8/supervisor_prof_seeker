19c8c765963ad5a63d8b3c3b7ee8e276d75d5d6b
Yaniv Ohayon <yanivohayon8@gmail.com>
2025-06-23 19:42:47 +0300

    remove deprecated functions

--- src/dataset_synthesize/synthesizer.py
+++ src/dataset_synthesize/synthesizer.py
@@ -1,12 +1,7 @@
 from src.utils import load_jsonl_to_dict_list
 from src.api_utils import verify_lunary_private_key
-from src.consts import LUNARY_DATASET_PATH
 import requests
 import json
-
-def read_dataset_from_file_(jsonl_path=LUNARY_DATASET_PATH):
-    return load_jsonl_to_dict_list(jsonl_path)
-
 
 LUNARY_END_POINT = "https://api.lunary.ai/v1"
 
@@ -51,25 +46,5 @@ def http_get_lunary_(url,  params=None, headers=None):
 def convert_bytes_to_str(data:bytes,decoding="utf-8"):
     return data.decode(decoding)
 
-
 def read_dataset(file_path:str=None):
     pass
-
-def get_runs_export_deprecated_(query_type="thread",export_format="jsonl"):
-    '''
-        View the parameters options
-        https://docs.lunary.ai/api-reference/runs/export-runs-data
-    '''
-    params = {
-        "type":query_type,
-        "exportFormat":export_format
-    }
-
-    response = http_get_lunary_(LUNARY_END_POINT,params=params)
-
-    content = response.content.decode("utf-8")
-
-    for line in content.splitlines():
-        if line.strip():
-            data =  json.loads(line)
-            yield data
