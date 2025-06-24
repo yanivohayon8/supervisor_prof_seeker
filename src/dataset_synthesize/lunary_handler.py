52acbdff9cff0cd5e41e61d6c988f5a3b1349f42
Yaniv Ohayon <yanivohayon8@gmail.com>
2025-06-24 20:34:37 +0300

    process_run_

--- src/dataset_synthesize/lunary_handler.py
+++ src/dataset_synthesize/lunary_handler.py
@@ -4,24 +4,39 @@ import requests
 import json
 
 LUNARY_END_POINT = "https://api.lunary.ai/v1"
-ALLOWED_LUNARY_TYPE_LLM_RUN_KEYS = {"id","projectId","feedback","parentFeedback","feedbacks","type","name","createdAt","endedAt","tokens","tags","input","output","metadata"}
+ALLOWED_LUNARY_TYPE_LLM_RUN_KEYS = {"id","projectId","feedback","parentFeedback","feedbacks","type","name","createdAt","endedAt","tokens","tags","metadata"} # "input","output"
 
-def write_to_db(collection_name:str,mongo_handler:MongoDBHandler):
+def write_to_db(collection_name:str,mongo_handler:MongoDBHandler,error_collection_name:str=None):
+    if not error_collection_name:
+        error_collection_name = collection_name + "_errors"
+
     mongo_handler.ensure_indexes(collection_name, [
         ("id", {"unique": True}),
         ("createdAt", {})
     ])
 
     for runs in get_runs():
-        filtered_runs = [
-            {k: run[k] for k in ALLOWED_LUNARY_TYPE_LLM_RUN_KEYS if k in run}
-            for run in runs
-        ]
-        if filtered_runs:
+        docs = []
+        error_docs = []
+
+        for run in runs:
             try:
-                mongo_handler.insert_many(collection_name,filtered_runs, ordered=False) # 
+                processed_run = process_run_(run)
+                docs.append(processed_run)
+            except Exception as e:
+                error_docs.append({**run,f"lunary_handler_error":str(e)})
+
+        if docs:
+            try:
+                mongo_handler.insert_many(collection_name,docs, ordered=False) # 
             except Exception as e:
                 print(f"Error inserting batch: {e}")
+        
+        if error_docs:
+            try:
+                mongo_handler.insert_many(error_collection_name,error_docs, ordered=False) # 
+            except Exception as e:
+                print(f"Error inserting error_docs batch: {e}")
 
 
 def get_runs():
@@ -40,13 +55,13 @@ def get_runs():
         content = json.loads(content_str)
         runs = content.get("data",[])
 
-def get_route_(*args):
-    arguments = [""] + list(args)
-    return "/".join(arguments)
-
 def get_url_(*args):
     route = get_route_(*args)
     return LUNARY_END_POINT + route
+
+def get_route_(*args):
+    arguments = [""] + list(args)
+    return "/".join(arguments)
 
 def http_get_lunary_(url,  params=None, headers=None):
     token = verify_lunary_private_key()
@@ -64,3 +79,37 @@ def http_get_lunary_(url,  params=None, headers=None):
 
 def convert_bytes_to_str(data:bytes,decoding="utf-8"):
     return data.decode(decoding)
+
+def process_run_(run:dict)->dict:
+    processed_run = {k: run[k] for k in ALLOWED_LUNARY_TYPE_LLM_RUN_KEYS if k in run}
+                
+    for message in run["input"]:
+        if message["role"] == "system":                    
+            if processed_run.get("input_retrieved_context"):
+                ValueError("Found two system messages")
+
+            processed_run["input_retrieved_context"] = get_context_(message["content"])
+        
+        if message["role"] == "user":
+            if processed_run.get("input_user",None):
+                raise ValueError(f"Two input messages of a user in run {run["id"]}")
+            
+            processed_run["input_user"] = message["content"]
+    
+    if not "input_retrieved_context" in processed_run.keys():
+        raise ValueError("Didn't found retrieved context")
+
+    if not run["output"].get("content"):
+        raise ValueError("Did not found output of assistant")
+    else:
+        processed_run["output_assistant"] = run["output"]["content"]
+
+    return processed_run
+
+
+def get_context_(run_input:str):
+    delimiter = "Context:\n"
+    start_index = run_input.index(delimiter) + len(delimiter)
+    return run_input[start_index:]
+
+
